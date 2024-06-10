e944dfadb0d1e6c4643ff90b427c72416febea68
Rakhymzhan <Rakhymzhan0001@gmail.com>
2024-06-10 17:38:21 +0500

    created list of posts

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { fetchPosts } from '../api';
 import Post from './Post';
+import { Link } from 'react-router-dom';
 
 const PostList = () => {
   const [posts, setPosts] = useState([]);
@@ -14,9 +15,13 @@ const PostList = () => {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       {posts.map(post => (
-        <Post key={post.id} post={post} />
+        <div className="post" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <Link to={`/post/${post.id}`}>Read More</Link>
+        </div>
       ))}
     </div>
   );
